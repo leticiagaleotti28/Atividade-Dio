8508d3ff248eeabe1ceda338526165a999a24606
leticiagaleotti28 <galeottileticia2805@gmail.com>
2022-07-14 00:20:46 -0300

    Atividade Atualizada

---
+++ src/TipodeDados/Atividade2.java
@@ -0,0 +1,56 @@
+package TipodeDados;
+
+public class Atividade2 {
+
+    public static void main(String[] args){
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        //int i1 = -10000000000;
+        int i2 = 28500;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 2004005000500055000L;
+
+        //float f1 = 4.5;
+        float f2 = 10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.84d;
+
+        char c1 = 'W';
+        //char c2 = 'Tw';
+        char c3 = '\u0057';
+
+        String st1 = "menino";
+        String st2 = "menina";
+        String st3 = "meninos querem brincar de futebol";
+
+        //String dt1 = "13/07/2022";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+}
